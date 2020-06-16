e37f94499bc118f930caa972e2f642e087caacac
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-16 19:15:42 +0300

    test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def handler(event, context):
     body = {
         'method': 'sendMessage',
         'text': data['message']['text'],
-        'chat_id': data['message']['from']['id'],
+        'chat_id': data['message']['user']['id'],
     }
     return {
         "statusCode": 200,
