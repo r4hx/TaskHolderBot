a299d33671917ec07a01ab1e5228f30baf40c505
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-16 22:22:05 +0300

    123

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 import json
-# import os
-
-# import boto3
-
-
-# session = boto3.session.Session()
-# s3 = session.client(
-#     service_name='s3',
-#     aws_access_key_id=os.getenv('USER_ACCESS_ID'),
-#     aws_secret_access_key=os.getenv('USER_ACCESS_SECRET'),
-#     region_name='ru-central1',
-#     endpoint_url='https://storage.yandexcloud.net'
-# )
 
 
 def handler(event, context):
