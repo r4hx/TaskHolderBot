e6d9529c37d88cc1179ce891fcbda4d949c522c6
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-16 17:25:02 +0300

    test s3 write function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import json
+import os
+import boto3
+
+
+session = boto3.session.Session()
+s3 = session.client(
+    service_name='s3',
+    aws_access_key_id=os.getenv('SERVICE_USER_ACCESS_ID'),
+    aws_secret_access_key=os.getenv('SERVICE_USER_ACCESS_SECRET'),
+    region_name='ru-central1',
+    endpoint_url='https://storage.yandexcloud.net'
+)
 
 
 def handler(event, context):
@@ -8,9 +20,18 @@ def handler(event, context):
         'text': data['message']['text'],
         'chat_id': data['message']['chat']['id'],
     }
+    s3.put_object(Bucket=os.getenv('YC_BUCKET'), Key=data['message']['chat']['id'], Body=data['message']['text'], StorageClass='COLD')
     return {
         "statusCode": 200,
         "headers": {'Content-Type': 'application/json'},
         "body": json.dumps(body),
         "isBase64Encoded": False
     }
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
