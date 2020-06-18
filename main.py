8b34d5cd550742c7f29e4539ce6086dabac34bfa
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-18 22:35:44 +0300

    testing

--- main.py
+++ main.py
@@ -2,30 +2,17 @@ import json
 
 
 def handler(event, context):
-    print(event)
-    if ('queryStringParameters' in event and 'name'
-            in event['queryStringParameters']):
-        name = event['queryStringParameters']['name']
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'text/plain'
-            },
-            "body": name,
-            "isBase64Encoded": True,
-        }
-    else:
-        data = json.loads(event["body"])
-        body = {
-            'method': 'sendMessage',
-            'text': data['message']['text'],
-            'chat_id': data['message']['chat']['id'],
-        }
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'text/plain'
-            },
-            "body": json.dumps(body),
-            "isBase64Encoded": True,
-        }
+    data = json.loads(event["body"])
+    body = {
+        'method': 'sendMessage',
+        'text': data['message']['text'],
+        'chat_id': data['message']['chat']['id'],
+    }
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json'
+        },
+        "body": json.dumps(body),
+        "isBase64Encoded": True,
+    }
