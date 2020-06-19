1c5f0817651aa0f21e747cbc5f3c3b137113cdf8
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-19 22:50:52 +0300

    test

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from yasdk import ObjectStorage
 def handler(event, context):
     yc = ObjectStorage()
     data = json.loads(event["body"])
-    body = {
-        'method': 'sendMessage',
-        'text': data['message']['text'],
-        'chat_id': data['message']['from']['id'],
-    }
-    print(data)
     yc.update_user_info(data)
-    return {
-        "statusCode": 200,
-        "headers": {
-            'Content-Type': 'application/json'
-        },
-        "body": json.dumps(body),
-        "isBase64Encoded": False,
-    }
+    print(data)
+    if data['message']['text'] == '/start':
+        return {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'application/json'
+            },
+            "body": 'Здарова ёпта 😀',
+            "isBase64Encoded": False,
+        }
+    else:
+        body = {
+            'method': 'sendMessage',
+            'text': data['message']['text'],
+            'chat_id': data['message']['from']['id'],
+        }
+        return {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'application/json'
+            },
+            "body": json.dumps(body),
+            "isBase64Encoded": False,
+        }
