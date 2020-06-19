137c701ef55a459377e0d432ef3c86891c116b2c
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-19 23:01:50 +0300

    test

--- main.py
+++ main.py
@@ -2,31 +2,28 @@ import json
 from yasdk import ObjectStorage
 
 
+def SendMessage(chat_id, text):
+    body = {
+        'method': 'sendMessage',
+        'text': text,
+        'chat_id': chat_id,
+    }
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json'
+        },
+        "body": json.dumps(body),
+        "isBase64Encoded": False,
+    }
+
+
 def handler(event, context):
     yc = ObjectStorage()
     data = json.loads(event["body"])
     yc.update_user_info(data)
     print(data)
     if data['message']['text'] == '/start':
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'application/json'
-            },
-            "body": 'Здарова ёпта 😀',
-            "isBase64Encoded": False,
-        }
+        return SendMessage(data['message']['from']['id'], 'Здарова ёпта 😀')
     else:
-        body = {
-            'method': 'sendMessage',
-            'text': data['message']['text'],
-            'chat_id': data['message']['from']['id'],
-        }
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'application/json'
-            },
-            "body": json.dumps(body),
-            "isBase64Encoded": False,
-        }
+        return SendMessage(data['message']['from']['id'], data['message']['text'])
