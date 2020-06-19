5f16b48e374b1b273c9e16a1f56614147e3fa745
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-20 01:53:08 +0300

    test

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def handler(event, context):
         return SendMessage(data['message']['from']['id'], messages['welcome'])
     else:
         try:
-            last_message = yc.get_user_info(data)['last_message'].decode('utf-8')
+            last_message = yc.get_user_info(data)['last_message']
         except KeyError:
             last_message = None
         finally:
