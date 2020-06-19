cb16b249586e4882d5f21e1574f6729b51e69315
krnlnx (MacOS/PyCharm) <r4hx@mail.ru>
2020-06-20 01:32:55 +0300

    test

--- main.py
+++ main.py
@@ -31,21 +31,22 @@ def SendMessage(chat_id, text):
 def handler(event, context):
     yc = ObjectStorage()
     data = json.loads(event["body"])
-    last_message = yc.get_user_info(data).get('last_message')
-    yc.update_user_info(data)
     if data['message']['text'] == '/start':
         yc.upload(data['message']['from']['id'], 'info.txt', '')
         yc.upload(data['message']['from']['id'], 'tasks.txt', '')
         return SendMessage(data['message']['from']['id'], messages['welcome'])
-    elif data['message']['text'] == 'Добавить задачу':
-        return SendMessage(data['message']['from']['id'], messages['task_add'])
-    elif data['message']['text'] == 'Посмотреть список':
-        return SendMessage(data['message']['from']['id'], messages['task_list'])
-    elif data['message']['text'] == 'Удалить':
-        return SendMessage(data['message']['from']['id'], messages['task_delete'])
     else:
-        if last_message == 'Добавить задачу':
-            yc.task_add(data)
-            return SendMessage(data['message']['from']['id'], messages['task_added'])
+        last_message = yc.get_user_info(data).get('last_message')
+        yc.update_user_info(data)
+        if data['message']['text'] == 'Добавить задачу':
+            return SendMessage(data['message']['from']['id'], messages['task_add'])
+        elif data['message']['text'] == 'Посмотреть список':
+            return SendMessage(data['message']['from']['id'], messages['task_list'])
+        elif data['message']['text'] == 'Удалить':
+            return SendMessage(data['message']['from']['id'], messages['task_delete'])
         else:
-            return SendMessage(data['message']['from']['id'], data['message']['text'])
+            if last_message == 'Добавить задачу':
+                yc.task_add(data)
+                return SendMessage(data['message']['from']['id'], messages['task_added'])
+            else:
+                return SendMessage(data['message']['from']['id'], data['message']['text'])
